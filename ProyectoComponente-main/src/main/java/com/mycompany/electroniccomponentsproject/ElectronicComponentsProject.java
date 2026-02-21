bb1c8080238aadb0adb3ccd57b31cb22199245e8
jxngel <juan.bocanegra@estudiantesunibague.edu.com>
2026-02-21 18:36:50 -0500

    feat: Implementar GUIMain con menú principal y ventanas para casos de uso (Adicionar, Listar, Consultar, Eliminar)

---
+++ ProyectoComponente-main/src/main/java/com/mycompany/electroniccomponentsproject/ElectronicComponentsProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.electroniccomponentsproject;
+
+import gui.GUIMain;
+
+/**
+ *
+ * @author jesus
+ */
+public class ElectronicComponentsProject {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new GUIMain().setVisible(true);
+        });
+    }
+}
